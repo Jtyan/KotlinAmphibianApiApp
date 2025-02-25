f2f590b895275aa17edf3afb1c7b0ad023bc4908
Joeltt <joelttm15@gmail.com>
2025-02-25 11:47:46 +0000

    feat: create an amphibianModel and a custom UUID serializer to serialize UUID as id

--- app/src/main/java/com/example/kotlinamphibiansapiapp/model/AmphibianModel.kt
+++ app/src/main/java/com/example/kotlinamphibiansapiapp/model/AmphibianModel.kt
@@ -1,5 +1,6 @@
 package com.example.kotlinamphibiansapiapp.model
 
+import com.example.kotlinamphibiansapiapp.util.UUIDSerializer
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
@@ -7,6 +8,7 @@ import java.util.UUID
 
 @Serializable
 data class AmphibianModel(
+    @Serializable(with = UUIDSerializer::class)
     val id: UUID = UUID.randomUUID(),
     val name: String,
     val type: String,
